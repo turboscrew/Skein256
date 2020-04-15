6056f8e55c0238b9f09a4ab3a3ee38a0b121c168
turboscrew <turboruuvi@hotmail.com>
2020-04-15 17:20:41 +0300

    Nicer output from test program.

--- skein_main.c
+++ skein_main.c
@@ -9,11 +9,13 @@
 #define SKEIN_TEST_MSG3
 
 
-uint64_t hash[4];
+//uint64_t hash[4];
+uint8_t hash[SKEIN_BLK_BYTES];
 
 #if defined(SKEIN_TEST_CONFIG) || defined(SKEIN_TEST_MSG1)
 uint8_t msg[] = {0xFF};
 #define SKEIN_MSG_LEN 1
+#define TST_MSG "MSG1"
 #endif
 
 #ifdef SKEIN_TEST_MSG2
@@ -22,6 +24,7 @@ uint8_t msg[] = {
 0xEF, 0xEE, 0xED, 0xEC, 0xEB, 0xEA, 0xE9, 0xE8, 0xE7, 0xE6, 0xE5, 0xE4, 0xE3, 0xE2, 0xE1, 0xE0
 };
 #define SKEIN_MSG_LEN 32
+#define TST_MSG "MSG2"
 #endif
 
 #ifdef SKEIN_TEST_MSG3
@@ -32,6 +35,7 @@ uint8_t msg[] = {
 0xCF, 0xCE, 0xCD, 0xCC, 0xCB, 0xCA, 0xC9, 0xC8, 0xC7, 0xC6, 0xC5, 0xC4, 0xC3, 0xC2, 0xC1, 0xC0
 };
 #define SKEIN_MSG_LEN 64
+#define TST_MSG "MSG3"
 #endif
 
 void skein_prthash(uint8_t *s)
@@ -48,28 +52,24 @@ void skein_prthash(uint8_t *s)
 
 int main()
 {
-	uint8_t *s;
-
 #ifdef SKEIN_PARTS_TESTS	
 	skein_parts_test();
 #else  /*SKEIN_PARTS_TESTS */
-	skein_hash((uint8_t *)hash, msg, SKEIN_MSG_LEN);
-	s = (uint8_t*)&hash;
-	skein_prthash(s);
-	printf("Should be:\n");
+	skein_hash(hash, msg, SKEIN_MSG_LEN);
+    printf("\nTesting with %s", TST_MSG);
+    printf("\nRessult is:");
+	skein_prthash(hash);
+	printf("\nResult should be:\n");
 
 #ifdef SKEIN_TEST_MSG1
-	printf("\nMSG1\n");
 	printf(" 0B 98 DC D1 98 EA 0E 50 A7 A2 44 C4 44 E2 5C 23\n");
 	printf(" DA 30 C1 0F C9 A1 F2 70 A6 63 7F 1F 34 E6 7E D2\n");
 #endif
 #ifdef SKEIN_TEST_MSG2
-	printf("\nMSG2\n");
 	printf(" 8D 0F A4 EF 77 7F D7 59 DF D4 04 4E 6F 6A 5A C3\n");
 	printf(" C7 74 AE C9 43 DC FC 07 92 7B 72 3B 5D BF 40 8B\n");
 #endif
 #ifdef SKEIN_TEST_MSG3
-	printf("\nMSG3\n");
 	printf(" DF 28 E9 16 63 0D 0B 44 C4 A8 49 DC 9A 02 F0 7A\n");
 	printf(" 07 CB 30 F7 32 31 82 56 B1 5D 86 5A C4 AE 16 2F\n");
 #endif
